d017d236f5b132a0127374a1e8985b9f85877b62
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2023-08-24 04:25:13 +0600

    add test cases and fix bugs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 interface Option {
-    length: number;
-    type: 'string' | 'number';
+    length?: number;
+    type?: 'string' | 'number';
 }
 
 const defaultOptions: Option = {
@@ -8,15 +8,31 @@ const defaultOptions: Option = {
     type: 'number',
 };
 
-export function generateVerificationCode({ length, type } = defaultOptions) {
+export function generateVerificationCode({
+    length = 6,
+    type = 'number',
+} = defaultOptions) {
+    if (!length) throw new TypeError('Length must be a defined');
     if (isNaN(length)) throw new TypeError('Length must be a number');
     if (length < 1) throw new TypeError('Length must be greater than zero');
+    if (type === 'number' && length > 16) {
+        throw new TypeError(
+            'Length must be less than or equal 17 digits for number type'
+        );
+    }
 
     const digits = '0123456789';
     let code = '';
 
     for (let i = 0; i < length; i++) {
-        code += digits.charAt(Math.floor(Math.random() * 9));
+        let randomIndex: number;
+        // if type is number then first digit should not be zero
+        if (type === 'number' && i === 0) {
+            randomIndex = Math.floor(Math.random() * 8) + 1;
+        } else {
+            randomIndex = Math.floor(Math.random() * 9);
+        }
+        code += digits.charAt(randomIndex).toString();
     }
     if (type === 'number') {
         return parseInt(code);
