8e24c2b491e4bc302c364b788ce09cccd35cb9b0
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2023-08-24 03:35:41 +0600

    add generate code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-export function generateVerificationCode() {
-  return 123456;
+interface Option {
+    length: number;
+    type: 'string' | 'number';
+}
+
+const defaultOptions: Option = {
+    length: 6,
+    type: 'number',
+};
+
+export function generateVerificationCode({ length, type } = defaultOptions) {
+    if (isNaN(length)) throw new TypeError('Length must be a number');
+    if (length < 1) throw new TypeError('Length must be greater than zero');
+
+    const digits = '0123456789';
+    let code = '';
+
+    for (let i = 0; i < length; i++) {
+        code += digits.charAt(Math.floor(Math.random() * 9));
+    }
+    if (type === 'number') {
+        return parseInt(code);
+    }
+    return code;
 }
